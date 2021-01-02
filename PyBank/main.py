8e1e59d557dba7bc5d3cfc9d64db42a50d068ce3
DrewMcB <drew.mcbride89@gmail.com>
2021-01-02 11:12:11 -0600

    added new folders

---
+++ PyBank/main.py
@@ -0,0 +1,5 @@
+import os
+import csv
+
+# Path to collect data from the Resources folder
+budget_csv = os.path.join('..', 'Resources', 'Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')

ef2b142469219e9f4640675abbfe6234d8249102
DrewMcB <drew.mcbride89@gmail.com>
2021-01-02 21:22:30 -0600

    partial homework completion

--- PyBank/main.py
+++
@@ -1,5 +0,0 @@
-import os
-import csv
-
-# Path to collect data from the Resources folder
-budget_csv = os.path.join('..', 'Resources', 'Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')

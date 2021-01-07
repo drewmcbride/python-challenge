aea38a77d894a9cf00da21320c48038a4f841ee2
DrewMcB <drew.mcbride89@gmail.com>
2021-01-06 23:59:47 -0600

    finished PyBank homework after being stuck

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,39 +1,52 @@
 import os
 import csv
-
-budget = os.path.join('Resources', 'budget_data.csv')
-
+budget = os.path.join("Resources","budget_data.csv")
 total_months = 0
 net_total = 0
-month_change = []
+previous_month = 0
+month_change = 0
+total_change = 0
 greatest_increase = 0
+greatest_increase_month = 0
 greatest_decrease = 0
-# def financial_analysis(budget_data):
-#     # For readability, it can help to assign your values to variables with descriptive names
-#     dates = budget_data[0]
-#     revenue = budget_data[1]
-# for row in budget_data:
-    
-    # print(revenue)
-
+greatest_decrease_month = 0
 with open(budget, 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     header = next(csv_reader)
     for row in csv_reader:
-        dates = row[0]
-        revenue = int(row[1])
+        current_month = row[0]
+        current_revenue = int(row[1])
+        #empty variables to hold greatest increase and decrease
 
-        total_months = total_months + 1
-        net_total = net_total + revenue
+
+        # total month calculation
+        total_months += 1
+
+        # calculation for first row
+        if previous_month == 0:
+            net_total = current_revenue
+
+        #calculation for remainder
+        if previous_month != 0:
+            net_total += current_revenue
+            month_change = current_revenue - previous_month
+            total_change += month_change
+            if month_change > greatest_increase:
+                greatest_increase = month_change
+                greatest_increase_month = current_month
+                print(greatest_increase)
+            elif month_change < greatest_decrease:
+                greatest_decrease = month_change
+                greatest_decrease_month = current_month
+        previous_month = current_revenue
         
-        average_change = net_total/total_months
+
+
+        average_change = total_change/85
+    print("Financial Analysis")
+    print("--------------------")
     print(f"Total Months: {total_months}")
     print(f"Total: ${net_total}")
-    print(f"Average Change: {average_change}")
-#     #   print(row[1])
-#     # financial_analysis(row)
-
-# print(f"Total Months: {total_months}")
-# print(f"Total: {net_total}")
-# print(row[1])
-
+    print(f"Average Change: ${round(average_change,2)}")
+    print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")   
+    print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,11 +1,29 @@
 import os
 import csv
 election = os.path.join('Resources', 'election_data.csv')
+
+# Need help building the function
+# def vote_count(row):
+# khan_total = 0
+# #     voter_id = int(row[0])
+# #     county = str(row[1])
+# #     candidate = str(row[2])
+# if candidate == "Khan":
+#             khan_total += 1
+# khan_total += 1
+#     return voter_id,country,candidate
+
+#We can let the function return the candidiate name
+
 total_votes = 0
 khan_total = 0
 correy_total = 0
 li_total = 0
 otooley_total = 0
+candidate_count = {}
+#candidate_count.keys()
+#candidate[row[2]] = 0
+winner = str()
 with open(election, 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     header = next(csv_reader)
@@ -14,27 +32,46 @@ with open(election, 'r') as csv_file:
         county = str(row[1])
         candidate = str(row[2])
         total_votes += 1
-        if candidate == "Khan":
-            khan_total += 1
-        elif candidate == "Correy":
-            correy_total += 1
-        elif candidate == "Li":
-            li_total += 1
-        elif candidate == "O'Tooley":
-            otooley_total += 1
-        khan_percent = round((khan_total / total_votes) * 100, 3)
-        correy_percent = round((correy_total / total_votes) * 100, 3)
-        li_percent = round((li_total / total_votes) * 100, 3)
-        otooley_percent = round((otooley_total / total_votes) * 100, 3)
-    
+        # <Check with if condition whether the row[2] is NOT present inside the KEYS of dictionary">
+        #     Append that key to the dictionary and we initalise the value of that key to zero
+        # <if name is present>>
+        #     Just add 1 to the value of the key
+
+        if candidate not in candidate_count.keys():
+            candidate_count[candidate] = 1
+        elif candidate in candidate_count.keys():
+            candidate_count[candidate] += 1
     print("Election Results")
     print("--------------------")
     print(f"Total Votes: {total_votes}")
     print("--------------------")
-    print(f"Khan: {khan_percent}% ({khan_total})")
-    print(f"Correy: {correy_percent}% ({correy_total})")
-    print(f"Li: {li_percent}% ({li_total})")
-    print(f"O'Tooley: {otooley_percent}% ({otooley_total})")
+    
+    for candidate in candidate_count.keys():
+        winner = max(candidate_count, key=candidate_count.get)
+        print(f"{candidate} {round(candidate_count[candidate]/total_votes * 100, 4)}% ({candidate_count[candidate]}) ")
     print("--------------------")
-    print(f"Winner : {winner}")
-    print("--------------------")
+    print(f"Winner: {winner}")
+        # if candidate == "Khan":
+        #     khan_total += 1
+        # elif candidate == "Correy":
+        #     correy_total += 1
+        # elif candidate == "Li":
+        #     li_total += 1
+        # elif candidate == "O'Tooley":
+        #     otooley_total += 1
+    #     khan_percent = round((khan_total / total_votes) * 100, 3)
+    #     correy_percent = round((correy_total / total_votes) * 100, 3)
+    #     li_percent = round((li_total / total_votes) * 100, 3)
+    #     otooley_percent = round((otooley_total / total_votes) * 100, 3)
+    
+    # print("Election Results")
+    # print("--------------------")
+    # print(f"Total Votes: {total_votes}")
+    # print("--------------------")
+    # print(f"Khan: {khan_percent}% ({khan_total})")
+    # print(f"Correy: {correy_percent}% ({correy_total})")
+    # print(f"Li: {li_percent}% ({li_total})")
+    # print(f"O'Tooley: {otooley_percent}% ({otooley_total})")
+    # print("--------------------")
+    # print(f"Winner : ")
+    # print("--------------------")
